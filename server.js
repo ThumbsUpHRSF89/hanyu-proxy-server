1db08fe2b07958d3a0f23fd8546e15dfc5d67d49
ysera11 <hanyuzhu94@gmail.com>
2018-03-10 12:18:00 -0800

    implement server to serve ms bundles

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const path = require('path');
+const request = require('request-promise');
 
 const app = express();
 const port = 8000;
 
 app.use('/product/:id/', express.static(path.join(__dirname, './public')));
+
+app.get('/ms/:serviceName', (req, res) => {
+  const { serviceName } = req.params;
+  const ports = {
+    RelatedProduct: 8001,
+    reviewSection: 8002,
+    productComparison: 8003,
+    itemDetails: 8004,
+  }
+
+  request(`http://localhost:${ports[serviceName]}/bundle.js`)
+    .then(body => res.send(body))
+    .catch((e) => {
+      res.sendStatus(500);
+    });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
